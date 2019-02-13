ef88d2d69d8edc7438465f1f69d1e0f6ae828381
MINERVA Information Security <github@minervais.com>
2019-02-13 00:52:30 -0500

    Update minervais.com.forceusercert.py

--- minervais.com.forceusercert.py
+++ minervais.com.forceusercert.py
@@ -2,7 +2,8 @@ import os
 import sys
 import shutil
 
-#this script requires jarsigner and apktool
+#this script requires jarsigner, apktool, and ~/.android/debug.keystore
+#~/.android/debug.keystore will only exist if you've built an APK with Android Studio in the past
 #tested on macOS
 
 if len( sys.argv ) == 1:
@@ -30,17 +31,12 @@ f.write(
 
 f.close()
 
-#some code loaned from overlaid.net
-def replace_words(base_text, device_values):
-    for key, val in device_values.items():
-        base_text = base_text.replace(key, val)
-    return base_text
-
 f = open(".out/AndroidManifest.xml", "r")
 
 xml = f.read()
 
-xml = xml.replace('<application','<application android:networkSecurityConfig="@xml/network_security_config"')
+if 'android:networkSecurityConfig="@xml/network_security_config"' not in xml:
+	xml = xml.replace('<application','<application android:networkSecurityConfig="@xml/network_security_config"')
 
 f.close()
 
@@ -56,7 +52,7 @@ newapk = newapk.replace('.apk','-patched.apk')
 
 os.system("apktool b -o ./" + newapk + " .out &>/dev/null")
 
-os.system("jarsigner -keystore .keys -storepass 111111 -keypass 111111 " + newapk + " minervais.com &>/dev/null")
+os.system("jarsigner -keystore ~/.android/debug.keystore -storepass android -keypass android " + newapk + " androiddebugkey &>/dev/null")
 
 if os.path.exists(".out"):
 	shutil.rmtree('.out', ignore_errors=True)
@@ -64,4 +60,4 @@ if os.path.exists(".out"):
 if os.path.exists(newapk):
 	print "all good! now just adb install " + newapk
 else :
-	print "couldn't created patched APK, something went wrong. sorry! :("
+	print "couldn't create patched APK, something went wrong. sorry! :("
